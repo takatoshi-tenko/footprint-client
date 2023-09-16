c68efe10208d33ac94f4752aedf6c8b956a05f78
takatoshi.tenko <chanten811@gmail.com>
2023-09-17 03:25:18 +0900

    add: vueファイルの作成

---
+++ pages/users.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Usersページ</h1>
+    <NuxtErrorBoundary>
+      <NuxtPage />
+      <template>
+        <p>{{ error }}</p>
+      </template>
+    </NuxtErrorBoundary>
+  </div>
+</template>
---
+++ pages/users/list.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>User Listページ</h1>
+    <h2>Counter</h2>
+    <p>Count: {{ counter }}</p>
+    <div>
+      <button @click="inc">+</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const counter = ref(0);
+const inc = () => {
+  throw new Error("エラー発生");
+  counter.value++;
+};
+</script>
